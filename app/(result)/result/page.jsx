35130db338d2c763e8108470b190df318dcda2bf
dadakhalandarb <dadakhalandarb0@gmail.com>
2023-11-02 11:53:32 +0530

    Modified and deleted the FemaleHairTestIntroPage copy

--- app/(result)/result/page.jsx
+++ app/(result)/result/page.jsx
@@ -1,6 +1,6 @@
 'use client'
-import ResultPageMainComponent from "../../../components/result/ResultPageMainComponent";
-import CartContextProvider from "../../../context/cart-store";
+import ResultPageMainComponent from "@/components/result/ResultPageMainComponent";
+import CartContextProvider from "@/context/cart-store";
 export default function page() {
   return (
     <CartContextProvider>
